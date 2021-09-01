7eae1df3494dbb16c7ac95061184912294f9283c
armin azhdehnia <arminmk18@gmail.com>
2021-09-01 15:14:01 -0400

    complete the generator

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,3 +1,9 @@
 from ._ae import *
+from ._gen import *
+from ._disc import *
+from ._gan import *
 
 AutoEncodeCustomV1 = CustomAutoEncoderV1
+DCGAN = DCGAN
+DCGANGenerator = DCGANGenerator
+DCGANDiscriminator = DCGANDiscriminator
