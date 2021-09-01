f468d83019e22488481bfa3ced43e94a54249659
armin azhdehnia <arminmk18@gmail.com>
2021-09-01 04:03:02 -0400

    refactor manage.py

--- src/loader/__init__.py
+++ src/loader/__init__.py
@@ -1,3 +1,6 @@
-from _zero_day import zero_data_dataset
+from ._zero_day import zero_data_dataset
+from ._ld import zero_data_load
+
 
 get_zero_dataset = zero_data_dataset
+get_zero_dataloader = zero_data_load
--- src/manage.py
+++ src/manage.py
@@ -4,8 +4,13 @@ from pathlib import Path
 # tools
 from .tools import convert_binary_to_image
 
+# model
+from src.trainer import AutoEncoderCV1Container
+from src.loader import get_zero_dataset
+
 operations = {
-    "conv_bin_to_im": "cvt_bin_im"
+    "conv_bin_to_im": "cvt_bin_im",
+    "auto_encoder": "train_ae"
 }
 
 
@@ -20,10 +25,14 @@ def main(arguments: argparse.Namespace) -> None:
         out_dir = Path(arguments.out)
         convert_binary_to_image(input_dir=in_dir, output_dir=out_dir)
 
+    elif arguments.op == operations.get("auto_encoder"):
+        print("")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", help="input path", action="append", nargs="+")
+    parser.add_argument("--input", help="input path", type=str, default="data/zero_day/images")
+    parser.add_argument("--input_lb", help="label csv file", type=str, default="data/zero_day/trainLabels.csv")
     parser.add_argument("--out", help="output path", type=str)
     parser.add_argument("--op", help="choose the operation", type=str, choices=list(operations.values()))
     parser.add_argument("--weight_decay", help="determine the weight decay", type=float, default=1e-5)
