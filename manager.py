4b2823bcd3fe7293f7fec9cd84b6bbbc62e490b3
armin azhdehnia <arminmk18@gmail.com>
2021-09-06 05:36:06 -0400

    checkpoint"

--- manager.py
+++ manager.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     parser.add_argument("--disc_feature_map", help="discriminator feature map", type=int, default=64),
 
     # train phase parameter
-    parser.add_argument("--epochs", help="number of epochs", type=int, default=50)
+    parser.add_argument("--epochs", help="number of epochs", type=int, default=20)
     parser.add_argument("--batch", help="batch size", type=int, default=64)
     parser.add_argument("--clip", help="clip weight for (differential privacy)", type=float, default=0.01)
     parser.add_argument("--c_repeat", help="critic repeat for (differential privacy)", type=int, default=5)
